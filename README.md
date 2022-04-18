05981d754934cd21810dd6a585a7a97fbbffb015
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-18 10:05:10 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomTextField
+
+A description of this package.
---
+++ Sources/CustomTextField/CustomTextField.swift
@@ -0,0 +1,6 @@
+public struct CustomTextField {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
